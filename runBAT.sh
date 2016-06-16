3fb6daa19030880b3b291dc34b8868074472160e
Karen Brems <karen.brems@hpe.com>
2016-06-16 13:09:55 -0700

    changed runtests.sh to runBAT.sh and only run non regression tests for build acceptance

--- runBAT.sh
+++ runBAT.sh
@@ -9,7 +9,7 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
 # the specific language governing permissions and limitations under the License.
 
-# run opveclib python 2 and 3 unit tests. stop after first failure
+# Run Build Acceptance Tests for opveclib on python 2 and 3. Sstop after first failure
 nose2-2.7 -F opveclib.test_tensorflow
 if [ ! $? -eq 0 ]; then
   echo "tensorflow integration tests failed"
@@ -20,13 +20,13 @@ if [ ! $? -eq 0 ]; then
   echo "example tests failed on Python 2"
   exit 1
 fi
-nose2-2.7 -F opveclib.test
+nose2-2.7 -F opveclib.test -A '!regression'
 if [ ! $? -eq 0 ]; then
   echo "unit tests failed on Python 2"
   exit 1
 fi
 # now run in python 3
-nose2-3.4 -F opveclib.test
+nose2-3.4 -F opveclib.test -A '!regression'
 if [ ! $? -eq 0 ]; then
   echo "unit tests failed on Python 3"
   exit 1
